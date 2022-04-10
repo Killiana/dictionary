c7e2803e3658b70c29d0ad4fa631fa5fd9e796a2
Killiana <illiana9875@gmail.com>
2022-04-10 13:02:24 +0100

    Received API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-alert("Hello");
+function displayWord(response) {
+  console.log(response.data);
+  console.log(response.data[0].word);
+  let wordEl = document.querySelector("#word");
+  wordEl.innerHTML = response.data[0].meanings[0];
+}
+
+function search(word) {
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+  axios.get(apiUrl).then(displayWord);
+}
+
+function getWord(event) {
+  event.preventDefault();
+  let input = document.querySelector("#input");
+  search(input.value);
+}
+
+let searchInput = document.querySelector("form");
+searchInput.addEventListener("submit", getWord);
